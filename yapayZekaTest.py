1e7dcc8319a4c35162b27c3131acff8954f4e760
Emine Coskun <ecoskun3334@gmail.com>
2024-10-24 19:56:45 +0300

    yapayzekaguncel

--- yapayZekaTest.py
+++ yapayZekaTest.py
@@ -1,71 +1,71 @@
-# def find_odd_numbers(lst):
+def find_odd_numbers(lst):
 
-#       odd_numbers = []  # Tek sayıları saklayacağımız yeni liste
-#       for num in lst:
-#         if num % 2 != 0:  # Eğer sayı 2'ye bölündüğünde kalan 0 değilse, tek sayıdır
-#             odd_numbers.append(num)  # Listeye ekle
-#       return odd_numbers  # Tek sayıların listesini döndür
+      odd_numbers = []  # Tek sayıları saklayacağımız yeni liste
+      for num in lst:
+        if num % 2 != 0:  # Eğer sayı 2'ye bölündüğünde kalan 0 değilse, tek sayıdır
+            odd_numbers.append(num)  # Listeye ekle
+      return odd_numbers  # Tek sayıların listesini döndür
 
 
-# print(find_odd_numbers([10, 15, 22, 33, 40, 55]))
+print(find_odd_numbers([10, 15, 22, 33, 40, 55]))
 
-# girdi = [10, 15, 22, 33, 40, 55]
-# tek_sayilar = []
+girdi = [10, 15, 22, 33, 40, 55]
+tek_sayilar = []
 
-# for i in girdi:
-#     if i%2 != 0:
-#         tek_sayilar.append(i)
+for i in girdi:
+    if i%2 != 0:
+        tek_sayilar.append(i)
 
-# print(tek_sayilar)
+print(tek_sayilar)
 
 #Soru 2: Verilen iki sayının en büyük ortak bölenini (EBOB) bulan bir fonksiyon yazın.
 
-# say1 = 30
-# say2 = 20
-# ebob = []
+say1 = 30
+say2 = 20
+ebob = []
 
-# i=1
-# while i <= say2:
-#    if say2 %i ==0: 
-#      if say1%i==0:
-#        ebob.append(i)
-#    i=i+1
-# print(ebob)
-# print(max(ebob))
+i=1
+while i <= say2:
+   if say2 %i ==0: 
+     if say1%i==0:
+       ebob.append(i)
+   i=i+1
+print(ebob)
+print(max(ebob))
 
 #3Verilen bir listede tekrar eden elemanları 
 # bulan ve bu elemanların kaç kez tekrar ettiğini gösteren bir fonksiyon yazın.
 
 # girdi = [1, 2, 2, 3, 4, 4, 4, 5]
 
-# checked = set()
+checked = set()
 
 
-# for i in girdi:
-#   if i not in checked:
-#     count = 0
-#     for j in girdi:
-#       if i==j:
-#         count+=1
+for i in girdi:
+  if i not in checked:
+    count = 0
+    for j in girdi:
+      if i==j:
+        count+=1
     
-#     print(f'listedeki: {i} sayisi: {count} kere yazilmistir.')
-#     checked.add(i)
+    print(f'listedeki: {i} sayisi: {count} kere yazilmistir.')
+    checked.add(i)
 
-# def count_duplicates(lst):
-#     # Elemanların kaç kez geçtiğini tutmak için sözlük
-#     counts = {}
+def count_duplicates(lst):
+    # Elemanların kaç kez geçtiğini tutmak için sözlük
+    counts = {}
     
-#     # Listeyi tarayalım
-#     for num in lst:
-#         if num in counts:
-#             counts[num] += 1  # Eğer eleman zaten varsa, sayacı arttır
-#         else:
-#             counts[num] = 1  # Yoksa, sayacı 1 yap
+    # Listeyi tarayalım
+    for num in lst:
+        if num in counts:
+            counts[num] += 1  # Eğer eleman zaten varsa, sayacı arttır
+        else:
+            counts[num] = 1  # Yoksa, sayacı 1 yap
 
-#     # Sadece tekrar eden elemanları döndürelim
-#     duplicates = {key: value for key, value in counts.items() if value > 1}
+    # Sadece tekrar eden elemanları döndürelim
+    duplicates = {key: value for key, value in counts.items() if value > 1}
     
-#     return duplicates
+    return duplicates
 
 # girdi = [1, 2, 2, 3, 4, 4, 4, 5]
 # print(count_duplicates(girdi)) 
@@ -74,11 +74,11 @@
 #4Bir cümledeki en uzun kelimeyi bulan bir fonksiyon yazın. 
 # Eğer birden fazla en uzun kelime varsa, bunları bir liste halinde döndürün.
 
-# def find_longest_word(sentence):
+def find_longest_word(sentence):
     
-#     words = sentence.split()
+    words = sentence.split()
 
-#     max_len = len(max(words, key=len))
+    max_len = len(max(words, key=len))
 
     # max(): Bu fonksiyon normalde, bir listedeki en büyük elemanı döndürür. Ancak biz burada, 
     # en büyük elemanı uzunluklarına göre bulmak istiyoruz.
@@ -91,38 +91,38 @@
     # kullanıyoruz. Yani önce en uzun kelimeyi buluyoruz, sonra onun karakter sayısını öğreniyoruz.
 
   
-#     longest_wors = [word for word in words if len(word)== max_len]
+    longest_wors = [word for word in words if len(word)== max_len]
 
-#     return longest_wors
+    return longest_wors
 
-# print(find_longest_word("Bu sınav zor ama çok öğretici olacak"))
+print(find_longest_word("Bu sınav zor ama çok öğretici olacak"))
 
 #5 Kullanıcıdan bir sayı alarak o sayıya kadar Fibonacci dizisini oluşturan bir Python fonksiyonu yazın.
-# def fibonacci(n):
-#     fib_list =[0,1]
+def fibonacci(n):
+    fib_list =[0,1]
 
-#     while len(fib_list)<n :
-#         next_eleman = fib_list[-1] + fib_list[-2]
-#         fib_list.append(next_eleman)
+    while len(fib_list)<n :
+        next_eleman = fib_list[-1] + fib_list[-2]
+        fib_list.append(next_eleman)
 
-#     return fib_list
+    return fib_list
 
-# sayi= int(input('bir sayi giriniz: '))
+sayi= int(input('bir sayi giriniz: '))
 
-# print(fibonacci(sayi))
+print(fibonacci(sayi))
   
 # #6Kullanıcıdan bir sayı alarak o sayının faktöriyelini hesaplayan bir Python fonksiyonu yazın.
-# def faktoriyel(n):
-#     result = 1
+def faktoriyel(n):
+    result = 1
 
-#     for i in range(n,0,-1):
-#         result*=i
+    for i in range(n,0,-1):
+        result*=i
 
-#     return result
+    return result
 
-# sayi= int(input('bir sayi giriniz: '))
+sayi= int(input('bir sayi giriniz: '))
 
-# print(faktoriyel(sayi))
+print(faktoriyel(sayi))
 
 #7 Kullanıcıdan bir sayı alın, bu sayıyı ters çevirin ve ters halini ekrana yazdırın.
 
